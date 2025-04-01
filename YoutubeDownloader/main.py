58af57d2c0ea2ac70778e9efd0dabaa8c0396b45
FlanGrande <francisjgrande@gmail.com>
2025-04-01 14:21:16 +0100

    Added ability to process multiple links at once

--- YoutubeDownloader/main.py
+++ YoutubeDownloader/main.py
@@ -4,19 +4,21 @@ from yt_dlp import YoutubeDL
 def download_youtube_video(url):
     try:
         ydl_opts = {
-            'format': 'bestvideo[height<=1080]+bestaudio/best[height<=1080]',
+            'format': 'bestvideo[height<=1081]+bestaudio/best[height<=1080]',
             'outtmpl': '%(title)s.%(ext)s',
         }
         with YoutubeDL(ydl_opts) as ydl:
             ydl.download([url])
-        print("Download completed.")
+        print(f"Download completed for: {url}")
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"An error occurred for {url}: {e}")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python download_video.py <YouTube_URL>")
-    else:
-        download_youtube_video(sys.argv[1])
-
-
+    if len(sys.argv) < 2:
+        print("Usage: python download_video.py <YouTube_URL1> [<YouTube_URL2> ...]")
+        sys.exit(1)
+    
+    # Skip the first argument (script name) and process all URLs
+    urls = sys.argv[1:]
+    for url in urls:
+        download_youtube_video(url)
